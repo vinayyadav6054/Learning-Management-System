1b24c6170f4ffe76e1bd0bb973de1273d470dbfa
vinayyadav6054 <yvinay7271@gmail.com>
2025-01-25 20:01:56 +0530

    Navbar Completed

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -17,8 +17,11 @@ const Navbar = () => {
         <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
           <div className='flex items-center gap-5'>
-            <button>Become Educator</button>
+            {  user &&<>
+              <button>Become Educator</button>
             | <Link to='/my-enrollments'>My Enrollments  </Link>
+            </>
+            }
           </div>
          {  user ? <UserButton/> :
           <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
@@ -26,14 +29,21 @@ const Navbar = () => {
 
         {/* For Phone Screens */}
         <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-          <div>
+      <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+        {user && (
+          <>
             <button>Become Educator</button>
-             |  <Link to='/my-enrollments'>My Enrollments</Link>
-          </div>
-          <button><img src={assets.user_icon} alt="" /></button>
+            | <Link to='/my-enrollments'>My Enrollments</Link>
+          </>
+        )}
         </div>
+        {
+          user ? <UserButton/> : <button onClick={()=> openSignIn()}><img src={assets.user_icon} alt="User Icon" /></button>
+        }
+        
+      </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
