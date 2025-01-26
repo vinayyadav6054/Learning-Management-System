7f1ea78e9745758f835682438dc749432f0273aa
vinayyadav6054 <yvinay7271@gmail.com>
2025-01-26 13:19:21 +0530

    Course Card Component and Functionality completed

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,26 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props)=>{
 
+
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const [allCourses, setAllCourses] = useState([])
+
+    //FetchAllCourses
+    const fetchAllCourese = async ()=>{
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(()=>{
+        fetchAllCourese()
+    },[])
+
     const value = {
-        
+        currency, allCourses
     }
 
     return(
